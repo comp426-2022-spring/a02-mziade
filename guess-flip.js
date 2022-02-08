b744dbb305ecc474db1c0494c0e8f7c10ff06023
Mia Ziade <mziade@live.unc.edu>
2022-02-07 20:49:30 -0500

    guess-flip

--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,12 @@
-/* import { createRequire } from 'module';
+import { createRequire } from 'module';
+import { flipACoin } from './modules/coin.mjs';
 const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2))
-args['number']
-const number = args.number || 1 */
+args['call']
+const call = args.call
+if (call == undefined) {
+    console.error(Error)
+    process.exit(1)
+} else {
+    console.log(flipACoin(call))
+}
