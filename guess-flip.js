51147604334409d769b90a492479e7f4b0154649
Mia Ziade <mziade@live.unc.edu>
2022-02-13 13:40:02 -0500

    err fix

--- guess-flip.js
+++ guess-flip.js
@@ -4,7 +4,7 @@ const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2))
 args['call']
 const call = args.call
-if (call == undefined) {
+if (call != "heads"|"tails") {
     console.error(Error)
     process.exit(1)
 } else {
