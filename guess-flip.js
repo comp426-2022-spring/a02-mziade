0c2444163bfcdc98cb42400624903f2ad5fccba5
Mia Ziade <mziade@live.unc.edu>
2022-02-13 13:45:14 -0500

    j

--- guess-flip.js
+++ guess-flip.js
@@ -5,7 +5,7 @@ const args = require('minimist')(process.argv.slice(2))
 args['call']
 const call = args.call
 if (call != "heads"|"tails") {
-    console.error(Error)
+    console.error("Error: no input")
     process.exit(1)
 } else {
     console.log(flipACoin(call))
