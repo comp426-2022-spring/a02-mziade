526e5b6331e2833ceceeb33380b969e22b754f56
Mia Ziade <mziade@live.unc.edu>
2022-03-02 15:36:40 -0500

    test

--- guess-flip.js
+++ guess-flip.js
@@ -4,15 +4,10 @@ const require = createRequire(import.meta.url);
 const args = require('minimist')(process.argv.slice(2))
 args['call']
 const call = args.call
-const userError = new TypeError("Error: no input.");
-const userError2 = new TypeError("Usage: node guess-flip --call=(heads|tails).");
 
 if (call == "heads" | call == "tails") {
     console.log(flipACoin(call))
 } else {
-    if(call == null){
-        console.error(userError.message)
-    } else{
-        console.error(userError2.message)
-    }
+        console.error("Error: no input.")
+        console.log("Usage: node guess-flip --call=(heads|tails).")
 }
